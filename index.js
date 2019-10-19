d9a423f694648aa0a8fcecdacc92f190d24d51b6
Gabriel Alcântara <gabrielalcantaratp@gmail.com>
2019-10-19 17:29:28 -0300

    tcc

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-var gplay = require('google-play-scraper');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const gplay = require("google-play-scraper");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
-const getData = async (name) => {
-  const data = await gplay.reviews({
-    appId: name,
-    sort: gplay.sort.RATING,
-  })
-  
-  const dataArray = await data.map(item => {
-    return item.text
-  }) 
-  const writer = await createCsvWriter({
-    path: './csv/'+name+dataArray.length+'.csv',
-    header: [
-      {id: 'text', title: dataArray},
-    ]
-  })
+const getData = async name => {
+	const data = await gplay.reviews({
+		appId: name,
+		sort: gplay.sort.NEWEST
+		// sort: gplay.sort.HELPFULNESS
+		// sort: gplay.sort.RATING
+	});
 
-  writer.writeRecords(dataArray)  
-}
-getData('id app uri')
+	const dataArray = await data.map(item => {
+		return item.text + ";";
+	});
+	const writer = await createCsvWriter({
+		path:
+			"./csv/NEWEST/" + name + " - " + dataArray.length + " - NEWEST" + ".csv",
+		recordDelimiter: "\n",
+		header: [{ id: "text", title: dataArray }]
+	});
+
+	writer.writeRecords(dataArray);
+};
+
+getData("forqan.tech.babypuzzles_edu.ads");
